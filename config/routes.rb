13bfbe20f7cc54bba3eb8d6ce19567e4c7ed44e8
fahmiammryhi <fahmiammry@gmail.com>
2025-12-10 16:39:07 +0700

    101225

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "books/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resource :books, only: :index
+  resources :books, only: :index
 end
